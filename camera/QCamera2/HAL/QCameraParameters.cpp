963d5bf21c5e4c99b9fd23b4d6cae15ae51722f7
Sultanxda <sultanxda@gmail.com>
2016-03-30 16:15:23 -0700

    QCamera2: Set denoise process plate to streamlined CbCr
    Change-Id: I66ab0562afad4a57ce7eafe336bae58117288040

--- camera/QCamera2/HAL/QCameraParameters.cpp
+++ camera/QCamera2/HAL/QCameraParameters.cpp
@@ -6637,6 +6637,7 @@ int32_t QCameraParameters::setRedeyeReduction(const char *redeyeStr)
  *==========================================================================*/
 cam_denoise_process_type_t QCameraParameters::getWaveletDenoiseProcessPlate()
 {
+#if 0
     char prop[PROPERTY_VALUE_MAX];
     memset(prop, 0, sizeof(prop));
     cam_denoise_process_type_t processPlate = CAM_WAVELET_DENOISE_CBCR_ONLY;
@@ -6661,6 +6662,8 @@ cam_denoise_process_type_t QCameraParameters::getWaveletDenoiseProcessPlate()
         }
     }
     return processPlate;
+#endif
+    return CAM_WAVELET_DENOISE_STREAMLINED_CBCR;
 }
 
 /*===========================================================================
