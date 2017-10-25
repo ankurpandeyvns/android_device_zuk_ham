4dfbbd541a2ee532c1d47972db62506e4bed3953
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2017-10-25 21:14:19 +0200

    ham: bluetooth: Bring bluetooth inline with CAF
     *Tag: LA.BF.1.1.3-01610-8x74.0
     *Note: Disable BLE_VND_INCLUDED as it causes bluetooth problems on O
    
    Signed-off-by: OrdenKrieger <OrdenKriegerEDR@googlemail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,5 +1,12 @@
 /*
+ *
+ *  Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ *  Not a Contribution, Apache license notifications and license are retained
+ *  for attribution purposes only.
+ *
+ * Copyright (C) 2012 The Android Open Source Project
  * Copyright (C) 2014 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,10 +23,15 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-
-#define BLUETOOTH_QTI_SW TRUE
-
 #define BTM_DEF_LOCAL_NAME   "Zuk Z1"
+// Disables read remote device feature
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
+#define MAX_L2CAP_CHANNELS    16
+// #define BLE_VND_INCLUDED   TRUE
+// skips conn update at conn completion
+#define BTA_BLE_SKIP_CONN_UPD  FALSE
+#define BLE_PERIPHERAL_ADV_NAME  FALSE
+#define BT_CLEAN_TURN_ON_DISABLED 1
 
 /* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
 #define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
