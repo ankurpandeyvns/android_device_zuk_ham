cf3584a76720f66129cce437f40ec07386968ced
Bruno Martins <bgcngm@gmail.com>
2017-11-24 12:41:44 +0000

    hidl: Include DRM & graphic allocator services
    Signed-off-by: ShubhamB6596 <shubhamprince111@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -37,7 +37,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
@@ -46,6 +47,7 @@ PRODUCT_PACKAGES += \
 # Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl 
