97e3ab85813318aeb6dd21a416bc0ab06c46d828
sb6596 <shubhamprince111@gmail.com>
2017-11-27 04:18:42 +0000

    ham: hidl: biometrics 2.0->2.1
    Signed-off-by: ShubhamB6596 <shubhamprince111@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -41,7 +41,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.0-service
+    android.hardware.biometrics.fingerprint@2.1-service
 
 # Graphics
 PRODUCT_PACKAGES += \
