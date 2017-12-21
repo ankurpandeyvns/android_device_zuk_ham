76696558e25eb854f7e5d1bec2582d13af6d856b
ShubhamB6596 <shubhamprince111@gmail.com>
2017-12-21 16:53:04 +0000

    ham: Add Bootanimation RES
    Signed-off-by: ShubhamB6596 <shubhamprince111@gmail.com>

--- aosp_ham.mk
+++ aosp_ham.mk
@@ -22,6 +22,9 @@ $(call inherit-product, device/zuk/ham/ham.mk)
 # Inherit some common AEX stuff.
 $(call inherit-product, vendor/aosp/common.mk)
 
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
 PRODUCT_NAME := aosp_ham
 PRODUCT_DEVICE := ham
 PRODUCT_MANUFACTURER := ZUK
