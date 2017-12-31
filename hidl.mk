18bdfbd38639e5c8bb7a6eae59820c9d00b98717
Paul Keith <javelinanddart@gmail.com>
2017-12-31 15:12:08 +0000

    msm8974: Use QTI HIDL power HAL
     * Let's use the new shiny binderized HAL in hardware/qcom/power
    
    Change-Id: I2abf4ca9b0b5a0cb031f12c400a1159962f5e94c
    
    Signed-off-by: ShubhamB6596 <shubhamprince111@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -69,8 +69,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service
+    android.hardware.power@1.0-service-qti
 
 # Keymaster
 PRODUCT_PACKAGES += \
