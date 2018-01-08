ab904cdb72881487458e2914bb0628875fa70250
ShubhamB6596 <shubhamprince111@gmail.com>
2018-01-08 05:48:53 +0000

    ham: Add Power HAL Service
    * Optimize dex2oat for boot
    
    Signed-off-by: ShubhamB6596 <shubhamprince111@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -68,7 +68,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl 
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 # Keymaster
 PRODUCT_PACKAGES += \
