f1334cf8bac6c296cf91efb495e34281cf27b51b
ShubhamB6596 <shubhamprince111@gmail.com>
2018-02-07 12:58:59 +0000

    Revert "ham: Enable charger suspend"
    This reverts commit 47797cb741994f4b11f5dcd76c766d94b2bf21a3.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,8 +94,6 @@ TARGET_USES_NON_TREBLE_CAMERA 	:= true
 
 # Charger
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
-BOARD_CHARGER_ENABLE_SUSPEND 	 := true
-BACKLIGHT_PATH 	 		 := /sys/class/leds/lcd-backlight/brightness
 
 # Tap to wake
 TARGET_TAP_TO_WAKE_NODE := /sys/devices/virtual/touch/tp_dev/gesture_on
