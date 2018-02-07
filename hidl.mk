012c2fc238f8d3682aedbe610629547c23177d51
ShubhamB6596 <shubhamprince111@gmail.com>
2018-02-07 12:44:56 +0000

    hidl: Make more HALS binderized
    Signed-off-by: ShubhamB6596 <shubhamprince111@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -24,21 +24,26 @@
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl 
+    android.hardware.audio@2.0-service \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@2.0-service
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
 
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-legacy \
     android.hardware.camera.provider@2.4-service \
-    camera.device@3.2-impl-legacy
+    camera.device@3.2-impl-legacy \
+    camera.device@1.0-impl-legacy
 
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
+
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
@@ -50,7 +55,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.memtrack@1.0-impl 
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
 
 # GPS
 PRODUCT_PACKAGES += \
@@ -74,7 +80,8 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
