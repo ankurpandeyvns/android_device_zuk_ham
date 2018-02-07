c140c57d6cfb4af492a81cfc73c2eb3a4d5689d0
ShubhamB6596 <shubhamprince111@gmail.com>
2018-02-07 13:01:08 +0000

    hidl: Use binderized Health HAL
    Signed-off-by: ShubhamB6596 <shubhamprince111@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -64,7 +64,9 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl
+    android.hardware.health@1.0-convert \
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
